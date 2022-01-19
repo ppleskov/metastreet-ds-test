89358eb1e05ded962675ebe98a97799ddc788146
Pavel Pleskov <ppleskov@gmail.com>
2022-01-19 14:59:25 +0300

    add frontend

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import os
 import pandas as pd
-import numpy as np
 
 from tkinter import *
 from tkcalendar import Calendar
+
+ROUND = 3
 
 def frontend(data):
     root = Tk()
     root.geometry("400x400")
 
-    max_date = np.max(data.keys())
-
+    max_date = max(data.keys())
     cal = Calendar(root,
                    selectmode='day',
-                   year=2020,
-                   month=5,
-                   day=22)
+                   year=max_date.year,
+                   month=max_date.month,
+                   day=max_date.day)
 
     cal.pack(pady=20)
 
+    # to fix formatting
+    data = {str(key.date()): data[key] for key in data}
+
     def get_data():
-        date = cal.get_date()
-        if date in data:
-            value.config(text=f"CV is {data[date]}")
+        current_date = str(cal.selection_get())
+
+        if current_date in data:
+            usd = round(data[current_date]['usd_cv'], ROUND)
+            eth = round(data[current_date]['eth_cv'], ROUND)
+
+            value.config(text=f"USD CV: {usd}\nETH CV: {eth}")
         else:
-            value.config(text=f"Date {date} has no CV")
+            value.config(text=f"There is no CV for {current_date}")
 
     Button(root, text="Calculate", command=get_data).pack(pady=20)
 
@@ -34,7 +41,7 @@ def frontend(data):
     root.mainloop()
 
 
-def calculate_collateral_value(df, q=0.25, c=0.15, w=30):
+def calculate_collateral_value(df, q=0.25, c=0.15, days=30):
     df["date"] = pd.to_datetime(df['day'], infer_datetime_format=True)
 
     cols = ["date", "eth_price", "usd_price"]
@@ -55,14 +62,13 @@ def calculate_collateral_value(df, q=0.25, c=0.15, w=30):
     # collateral value
     for currency in ["eth", 'usd']:
         dt[f"{currency}_product"] = dt[f"{currency}_price"] * dt["cnt"]
-        dt[f"{currency}_cv"] = dt[f"{currency}_product"].rolling(w).sum() / dt["cnt"].rolling(w).sum()
+        dt[f"{currency}_cv"] = dt[f"{currency}_product"].rolling(days).sum() / \
+                               dt["cnt"].rolling(days).sum()
 
     dt = dt[~dt["usd_cv"].isnull()]
-    dt.index = dt.index.date.astype(str)
+    # dt.index = dt.index.date.astype(str)
     data = dt[["usd_cv", "eth_cv"]].to_dict("index")
-
-    print(data)
-
+    # print(data)
     return data
 
 
@@ -70,15 +76,14 @@ def backend(path):
     assert os.path.isfile(path), f"Pls make sure {path} exists"
 
     df = pd.read_csv(path)
-    calculate_collateral_value(df)
+    data = calculate_collateral_value(df)
 
-    return df
+    return data
 
 
 def main(path="../cryptopunks_01-14-2022_13-55-22_downloaded.csv"):
     data = backend(path)
-    # print(data["date"])
-    # frontend(data)
+    frontend(data)
 
 
 if __name__ == "__main__":
